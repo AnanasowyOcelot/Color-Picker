980c651575b5a4331b643b99aba2ccf4f69e298e
AnanasowyOcelot <lukasz.woroniecki@gmail.com>
2014-07-24 17:41:17 +0200

    better color selection

--- index.php
+++ index.php
@@ -8,9 +8,38 @@ function rgb2html($r, $g, $b)
     $g = intval($g);
     $b = intval($b);
 
-    $r = dechex($r);
-    $g = dechex($g);
-    $b = dechex($b);
+    $arr = [$r, $g, $b];
+
+    sort($arr);
+
+    $median = $arr[1];
+
+    $highestVal = max($r, $g, $b);
+    $lowestVal = min($r, $g, $b);
+
+    $nr=0;
+    $ng=0;
+    $nb=0;
+
+    if ($r == $highestVal) {
+        $nr = 255;
+    } else if ($g == $highestVal) {
+        $ng = 255;
+    } else if ($b == $highestVal) {
+        $nb = 255;
+    }
+
+    if ($r == $lowestVal) {
+        $nr = 0;
+    } else if ($g == $lowestVal) {
+        $ng = 0;
+    } else if ($b == $lowestVal) {
+        $nb = 0;
+    }
+
+    $r = dechex($nr);
+    $g = dechex($ng);
+    $b = dechex($nb);
 
     $color = (strlen($r) < 2 ? '0' : '') . $r;
     $color .= (strlen($g) < 2 ? '0' : '') . $g;
@@ -18,52 +47,64 @@ function rgb2html($r, $g, $b)
     return '#' . $color;
 }
 
-$colors = [];
+$urls = ['spring-snowdrops-wallpaper-flowers-32897799-1920-1080.jpg', 'uRBtadp.jpg'];
+$url = 'uRBtadp.jpg';
 
-$im = imagecreatefromjpeg("spring-snowdrops-wallpaper-flowers-32897799-1920-1080.jpg");
-//$im = imagecreatefrompng("piq_22677_400x400.png");
+foreach ($urls as $url){
 
-$answerToEverything = 15;
+    echo '<img src="',$url,'" style="height:200; width:200;"><br/>';
 
-list($width, $height) = getimagesize("spring-snowdrops-wallpaper-flowers-32897799-1920-1080.jpg");
-$image_p = imagecreatetruecolor($answerToEverything, $answerToEverything);
+    $colors = [];
 
-imagecopyresampled($image_p, $im, 0, 0, 0, 0, $answerToEverything, $answerToEverything, $width, $height);
-//imagefilter($image_p, IMG_FILTER_PIXELATE, 2);
+    $im = imagecreatefromjpeg($url);
 
-$im = $image_p;
+    $answerToEverything = 15;
 
-for ($y = 0; $y < imagesy($im); $y++) {
-    for ($x = 0; $x < imagesx($im); $x++) {
-        $rgb = imagecolorat($im, $x, $y);
+    list($width, $height) = getimagesize($url);
+    $url_p = imagecreatetruecolor($answerToEverything, $answerToEverything);
 
-        $r = ($rgb >> 16) & 0xFF;
-        $g = ($rgb >> 8) & 0xFF;
-        $b = $rgb & 0xFF;
+    imagecopyresampled($url_p, $im, 0, 0, 0, 0, $answerToEverything, $answerToEverything, $width, $height);
+    imagefilter($url_p, IMG_FILTER_PIXELATE, 2);
 
-        $color = rgb2html($r, $g, $b);
+    $im = $url_p;
 
-        if (!array_key_exists($color, $colors)) {
-            $colors[$color] = 1;
-        } else if (array_key_exists($color, $colors)) {
-            $colors[$color] = $colors[$color] + 1;
+    for ($y = 0; $y < imagesy($im); $y++) {
+        for ($x = 0; $x < imagesx($im); $x++) {
+            $rgb = imagecolorat($im, $x, $y);
+
+            $r = ($rgb >> 16) & 0xFF;
+            $g = ($rgb >> 8) & 0xFF;
+            $b = $rgb & 0xFF;
+
+
+
+            $color = rgb2html($r, $g, $b);
+
+            if (!array_key_exists($color, $colors)) {
+                $colors[$color] = 1;
+            } else if (array_key_exists($color, $colors)) {
+                $colors[$color] = $colors[$color] + 1;
+            }
+
+        }
+    }
+
+    arsort($colors);
+    $colors = array_keys($colors);
+
+//if (($key = array_search('#000000', $colors)) !== false) {
+//    array_splice($colors, $key, 1);
+//}
+
+    for ($i = 0; $i < $answerToEverything * $answerToEverything; $i++) {
+        if (isset($colors[$i])) {
+            echo '<div style="height:50px; width:50px; background-color:' . $colors[$i] . '; display: inline-block;"></div>';
+
         }
 
     }
-}
-
-arsort($colors);
-$colors = array_keys($colors);
-
-if (($key = array_search('#000000', $colors)) !== false) {
-    array_splice($colors, $key, 1);
-}
-
-for ($i = 0; $i < $answerToEverything * $answerToEverything; $i++) {
-    if (isset($colors[$i])) {
-        echo '<div style="height:50px; width:50px; background-color:' . $colors[$i] . '; display: inline-block;"></div>';
-
-    }
+    echo "<br/>";
+    echo "<br/>";
 
 }
 
